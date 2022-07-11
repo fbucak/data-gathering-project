f1025d2823d7cb8ff5b286f6d0bcf4bacecf7634
fbucak <bucak.fth@gmail.com>
2022-07-11 18:42:00 +0200

    datalar ui kismina aktarildi

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import weather
 import requests
 import json
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel
+from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
 from PyQt5.QtGui import QPixmap, QImage
 import sys
 import os
+import psycopg2
 
 class MainUI(QMainWindow):
     def __init__(self):
@@ -17,6 +19,9 @@ class MainUI(QMainWindow):
         self.date.setText(weather.date(city))      
         self.date1.setText(weather.date1(city))
         self.date2.setText(weather.date2(city))
+        self.start_button.clicked.connect(self.start)
+        self.tableWid.setColumnWidth(0,380)
+        self.tableWid.clicked.connect(self.Action)
                     
         self.minTemp.setText(weather.min_temp(city))
         self.minTemp1.setText(weather.min_temp1(city))
@@ -39,6 +44,59 @@ class MainUI(QMainWindow):
         self.icon1.setPixmap(QPixmap(image))
         image.loadFromData(requests.get(str(weather.icon_url2(city))).content)
         self.icon2.setPixmap(QPixmap(image))
+
+    def start(self):
+             
+            conn = psycopg2.connect(host= 'localhost',database = 'Autoscout24',user = 'postgres',password = '12345')
+            cur = conn.cursor()
+            
+
+        
+            qry = "select brand_model,year,plate from cars"
+            cur.execute(qry)
+            self.cars = cur.fetchall()
+        
+            self.tableWid.setRowCount(len(self.cars))
+            self.total_count_label.setText(str(len(self.cars)))
+            row = 0
+            for i in self.cars:
+                self.tableWid.setItem(row, 0, QtWidgets.QTableWidgetItem(i[0]))
+                self.tableWid.setItem(row, 1, QtWidgets.QTableWidgetItem(i[1]))
+                self.tableWid.setItem(row, 2, QtWidgets.QTableWidgetItem(i[2]))
+                row = row+1
+
+            conn.commit()
+
+    def Action(self):
+        conn= psycopg2.connect(host= 'localhost',
+        database = 'Autoscout24',
+        user = 'postgres',password = '12345')
+        cur = conn.cursor()
+
+        index=(self.tableWid.selectionModel().currentIndex())
+        value=index.row()+1
+
+        cur.execute("select brand_model,plate,km,year,price,province,image from cars where id = %s;",(value,))
+        car = cur.fetchall()
+        for c in car:
+            self.modellabel.clear()
+            self.modellabel.insert(c[0])
+            self.kmlabel.clear()
+            self.kmlabel.insert(c[2])
+            self.pricelabel.clear()
+            self.pricelabel.insert(c[4])
+            self.yearlabel.clear()
+            self.yearlabel.insert(c[3])
+            self.provincelabel.clear()
+            self.provincelabel.insert(c[5])
+            self.platelabel.clear()
+            self.platelabel.insert(c[1])
+            
+
+        conn.commit()
+
+
+
         
             
 
